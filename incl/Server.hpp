3990a36cdf262f03e0d0d1d2b1b0ef3ca40c29c7
Oliver Krahl <krahlolli@gmail.com>
2024-10-02 13:38:49 +0200

    server structure added

--- incl/Server.hpp
+++ incl/Server.hpp
@@ -4,10 +4,16 @@
 #include "Data.hpp"
 #include <string>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 class Server
 {
 	public:
+
 		Server(const ServerConf& config);
 		~Server();
 
@@ -15,7 +21,14 @@ class Server
 		void run();
 
 	private:
+
 		ServerConf config;
+		int server_fd;
+
+		bool createSocket();
+		bool bindSocket();
+		bool listenOnSocket();
+		void acceptConnections();
 };
 
-#endif // SERVER_HPP
+#endif
