90d9f51b11818612704c7bc5fa651029f7ef6fdd
elecarlier <eleonorecarlier@hotmail.be>
2024-10-10 17:41:04 +0200

    add parsing to main

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,36 +6,38 @@
 /*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:18:42 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/10/10 17:02:52 by ecarlier         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:11:20 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/TcpServer.hpp"
-
+#include "../includes/Location.hpp"
+#include "../includes/Parser.hpp"
+#include "../includes/ServerConfig.hpp"
 
 int main()
 {
-	TcpServer server;
+	// TcpServer server;
 
-    // Parser parser;
-    // std::stringstream configBuffer;
+    Parser parser;
+    std::stringstream configBuffer;
 
-    // if (!parser.readFile("server_conf/invalid.conf", configBuffer)) {
-    //     return 1;
-    // }
-    // if (!parser.ParseConfigStream(configBuffer)) {
-    //     std::cerr << "Erreur lors de l'analyse de la configuration." << std::endl;
-    //     return 1;
-    // }
+    if (!parser.readFile("server_conf/basic.conf", configBuffer)) {
+        return 1;
+    }
+    if (!parser.ParseConfigStream(configBuffer)) {
+        std::cerr << "Erreur lors de l'analyse de la configuration." << std::endl;
+        return 1;
+    }
 
-	try
-	{
-		server.startServer();
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	// try
+	// {
+	// 	server.startServer();
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
 
 
 	return 0;
