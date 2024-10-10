bed893134d7ca045fd61959d287558c401f9742b
elecarlier <eleonorecarlier@hotmail.be>
2024-10-10 15:31:49 +0200

    spo

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:18:42 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/10/10 15:26:58 by ecarlier         ###   ########.fr       */
+/*   Updated: 2024/10/10 15:28:57 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,26 @@
 
 int main() {
 
-    Parser parser;
-    std::stringstream configBuffer;
+    //Parser parser;
+    TcpServer server;
+    // std::stringstream configBuffer;
 
-    if (!parser.readFile("server_conf/invalid.conf", configBuffer)) {
-        return 1;
-    }
-    if (!parser.ParseConfigStream(configBuffer)) {
-        std::cerr << "Erreur lors de l'analyse de la configuration." << std::endl;
-        return 1;
-    }
+    // if (!parser.readFile("server_conf/invalid.conf", configBuffer)) {
+    //     return 1;
+    // }
+    // if (!parser.ParseConfigStream(configBuffer)) {
+    //     std::cerr << "Erreur lors de l'analyse de la configuration." << std::endl;
+    //     return 1;
+    // }
 
-	// try
-	// {
-	// 	server.startServer();
-	// }
-	// catch (const std::exception &e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	try
+	{
+		server.startServer();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
 
     return 0;
