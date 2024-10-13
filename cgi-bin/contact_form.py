d6a7dbd031364c064a4a905890c09fc80f3752de
gabodinho <gabodinho80@gmail.com>
2024-10-14 01:20:42 +0200

    modified python script

--- cgi-bin/contact_form.py
+++ cgi-bin/contact_form.py
@@ -22,6 +22,16 @@ age = parsed_data['age'][0]
 print("Content-Type: text/html\n")  # CGI script needs to specify content type in the response
 print("<html><head><title>Contact Form Submission</title></head><body>")
 print("<h2>Thank you, {}!</h2>".format(name))
-print("<p>You are {} years old; very impressive!</p>".format(age))
+print("<p>You are {} years old".format(age))
+if int(age) < 0:
+	print("... are you sure?</p>")
+elif int(age) < 10:
+	print("; that's cool!</p>")
+elif int(age) < 30:
+	print("; well you are still young ;) </p>")
+elif int(age) < 90:
+	print("; time flies huh?</p>")
+else:
+	print("; I bow before your wisdom!</p>")
 print("<p>We will reach out to you at {}</p>".format(email))
 print("</body></html>")
