c2aa1ccdec308d74f10b0c98ace6a28870894b0b
elecarlier <eleonorecarlier@hotmail.be>
2024-10-21 18:52:44 +0200

    spo

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,20 +6,18 @@
 /*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:18:42 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/10/21 18:49:34 by ecarlier         ###   ########.fr       */
+/*   Updated: 2024/10/21 18:51:26 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/TcpServer.hpp"
+#include "../includes/Location.hpp"
+#include "../includes/Parser.hpp"
+#include "../includes/ServerConfig.hpp"
+#include "../includes/Webserver.hpp"
 
-#include "../includes/TcpServer.hpp"
-
-#include "../includes/TcpServer.hpp"
-
-
-int main()
+int main(int argc, char **argv)
 {
-	//TcpServer server;
     (void)argv;
 
     if (argc > 2)
@@ -35,24 +33,26 @@ int main()
     Parser parser;
     std::stringstream configBuffer;
 
-    if (!parser.readFile("server_conf/basic.conf", configBuffer)) {
+    if (!parser.readFile("server_conf/test.conf", configBuffer)) {
         return 1;
     }
     if (!parser.ParseConfigStream(configBuffer)) {
-        std::cerr << "Erreur lors de l'analyse de la configuration." << std::endl;
+        std::cerr << "Error while parsing." << std::endl;
         return 1;
     }
 
-    server = parser.getFirstServer();
-	try
-	{
+    std::vector<ServerConfig> servers = parser.getServers();
 
-		server.startServer();
-	}
-	catch (const std::exception &e)
+    try
+    {
+            Webserver webserver(servers);
+             webserver.initializeServers();
+    }
+    catch (const std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
 	return 0;
 }
+
